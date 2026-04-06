dcd3a5e2e98aa61c7f73f840fa627183f0d547d8
chae1s <chae1zd.b@gmail.com>
2026-04-06 09:57:08 +0900

    feat: 카프카에 저장된 데이터 MySQL에 저장

--- src/main/java/com/pageon/backend/config/KafkaConfig.java
+++ src/main/java/com/pageon/backend/config/KafkaConfig.java
@@ -1,13 +1,15 @@
 package com.pageon.backend.config;
 
+import com.pageon.backend.dto.payload.ActionLogEvent;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.core.*;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.HashMap;
@@ -36,5 +38,18 @@ public class KafkaConfig {
     @Bean
     public KafkaTemplate<String, Object> kafkaTemplate() {
         return new KafkaTemplate<>(producerFactory());
+    }
+
+    @Bean
+    public ConsumerFactory<String, ActionLogEvent> consumerFactory() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29092");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "foo-1");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+
+        JsonDeserializer<ActionLogEvent> deserializer = new JsonDeserializer<>(ActionLogEvent.class, false);
+
+        return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(), deserializer);
     }
 }
--- src/main/java/com/pageon/backend/dto/payload/ActionLogEvent.java
+++ src/main/java/com/pageon/backend/dto/payload/ActionLogEvent.java
@@ -2,6 +2,7 @@ package com.pageon.backend.dto.payload;
 
 import com.pageon.backend.common.enums.ActionType;
 import com.pageon.backend.common.enums.ContentType;
+import com.pageon.backend.entity.ContentActionLog;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -18,4 +19,14 @@ public class ActionLogEvent {
     private ContentType contentType;
     private ActionType actionType;
     private Integer ratingScore;
+
+    public ContentActionLog toEntity() {
+        return ContentActionLog .builder()
+                .userId(this.userId)
+                .contentId(this.contentId)
+                .contentType(this.contentType)
+                .actionType(this.actionType)
+                .ratingScore(this.ratingScore)
+                .build();
+    }
 }
